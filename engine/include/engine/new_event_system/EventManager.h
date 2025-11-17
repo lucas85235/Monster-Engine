240ff96c5ec47811ffc79fb53e19c9663eaf1620
Rafael Aragao Morais <ram.morais50@gmail.com>
2025-11-17 15:38:46 -0400

    Event system with a queue
    Signed-off-by: Rafael Aragao Morais <ram.morais50@gmail.com>

--- engine/include/engine/new_event_system/EventManager.h
+++ engine/include/engine/new_event_system/EventManager.h
@@ -16,12 +16,25 @@ class EventManager {
     }
 
     static void Publish(const NewEvent& event) {
+        // adds in the event_queue_ to be dispatched in a late time in the same frame
+        event_queue_.push_back(event);
 
-        // executes instantly
-        for (const auto& event_function : listeners_[event.type]) { event_function(event); }
+        if (event_queue_.size() == 10) {
+            Dispatch();
+        }
+    }
+
+    static void Dispatch() {
+        for (auto event : event_queue_) {
+            for (const auto& listener : listeners_[event.type]) {
+                listener(event);
+            }
+        }
+        event_queue_.clear();
     }
 
    private:
+    inline static std::vector<NewEvent> event_queue_;
     inline static std::unordered_map<EventType, std::vector<EventCallbackFn>> listeners_;
 };
 }  // namespace se
