28adc893e1726f7bd056f383ee0da96f53ab60d0
Rafael Aragao Morais <ram.morais50@gmail.com>
2025-12-16 21:07:26 -0400

    improve the window to always open in the center of screen when not in fullscreen mode
    Signed-off-by: Rafael Aragao Morais <ram.morais50@gmail.com>

--- engine/src/Window.cpp
+++ engine/src/Window.cpp
@@ -96,6 +96,13 @@ void Window::Init() {
         window_handle_ =
             glfwCreateWindow(static_cast<int>(spec_.Width), static_cast<int>(spec_.Height),
                              spec_.Title.c_str(), nullptr, nullptr);
+
+        int width, height;
+        GLFWmonitor* primary_monitor = glfwGetPrimaryMonitor();
+        glfwGetMonitorWorkarea(primary_monitor, nullptr, nullptr, &width, &height);
+
+        // to open in the center of screen
+        glfwSetWindowPos(window_handle_, width/2 - spec_.Width/2, height/2 - spec_.Height/2);
     }
 
     if (!window_handle_) {
